9ba2c9429866b3b3261e05aa1027043ac55b6581
colby <colby@10.1.10.3>
2016-10-25 09:26:52 -0500

    Made copy-paste modifications as requested

---
+++ Lab7b/src/lab7b/additions.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab7b;
+
+/**
+ *
+ * @author colby
+ */
+public class additions {
+
+		if(currentCell == 5 && advance == true)
+		{
+			//.	.	.
+		}
+ 
+		if(currentCell == 6 && advance == true)
+		{
+			//
+		}
+
+		if(currentCell == 9 && advance == true)
+		{
+			//.	.	.
+		}
+    
+}
